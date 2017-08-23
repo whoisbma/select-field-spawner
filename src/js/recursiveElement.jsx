139e4eafe1186e71e7fcfba3c1720ee9d7988745
Bryan Ma <bryanhma@gmail.com>
2017-08-23 12:34:58 -0400

    recursive element

--- src/js/recursiveElement.jsx
+++ src/js/recursiveElement.jsx
@@ -5,8 +5,11 @@ import { arrayData } from './data.jsx';
 export class RecursiveElement extends React.Component {
 	constructor(props) {
 		super(props);
-	
+		
+		// still need to know if its the first element or not, whether or not its a form or whatever other kind of element.
+		this.isRoot = props.setParentForFutureRemoval ? false : true;
 		// this.state = { }
+
 	}
 
 	componentWillMount() {}
@@ -18,3 +21,7 @@ export class RecursiveElement extends React.Component {
 	componentWillUnmount() {}
 
 } 
+
+RecursiveElement.defaultProps = {
+	setParentForFutureRemoval: null, // recursive elements need to set their parent for future removal.
+}
