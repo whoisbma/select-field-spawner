956f0b80e0bb814eec0a4bb90b05ebc4a7f1c5d0
Bryan Ma <bryanhma@gmail.com>
2017-09-03 07:51:58 -0400

    slow data changes

--- src/js/data.jsx
+++ src/js/data.jsx
@@ -1,3 +1,20 @@
+/*
+option data model:
+	text
+	target
+	postText
+	postImage
+	postLink
+	post..what?
+	video?
+	gif?
+	some special behavior for certain kinds of elements?
+	inputField prompt?
+*/
+// what happens when a single option has more than one kind of thing? 
+// add a plus button for more info? more stuff to follow?
+
+
 module.exports = {
 	data: [
 		{
@@ -14,7 +31,7 @@ module.exports = {
 			name: 'is',
 			index: 1,
 			options: [
-				{ text: 'a technologist', target: 2, postText: 'at museum design firm Gallagher and Associates,' },
+				{ text: 'a technologist', target: 2, postText: 'at museum design firm Gallagher and Associates in New York,' },
 				{ text: 'an artist', target: 3, postText: null },
 				{ text: 'an educator', target: 4, postText: 'at Parsons School of Design, teaching'},
 			]
@@ -23,8 +40,8 @@ module.exports = {
 			name: 'a technologist',
 			index: 2,
 			options: [
-				{ text: 'working on', target: null, postText: null },
-				{ text: 'working with', target: null, postText: 'full stack native technologies such as' },
+				{ text: 'working with', target: 5, postText: 'full stack web and native technologies such as' },
+				{ text: 'working on', target: 6, postText: 'interactive exhibits and installations like' },
 			]
 		},
 		{
@@ -39,16 +56,28 @@ module.exports = {
 			name: 'an educator',
 			index: 4,
 			options: [
-				{ text: 'Processing', target: null, postText: null },
-				{ text: 'Javascript', target: null, postText: null },
-				{ text: 'P5.js', target: null, postText: null },
-				{ text: 'Node.js', target: null, postText: null },
-				{ text: 'C++', target: null, postText: null },
-				{ text: 'OpenFrameworks', target: null, postText: null },
-				{ text: 'generative design', target: null, postText: null },
-				{ text: 'computational art', target: null, postText: null },
-				{ text: 'expressive interfaces', target: null, postText: null },
+				{ text: 'generative design and computational art', target: null, postText: null },
+				{ text: 'experimental, playful, and expressive interfaces', target: null, postText: null },
 				{ text: 'game design and development', target: null, postText: null },
+			]
+		},
+		{
+			name: 'working with',
+			index: 5,
+			options: [
+				{ text: 'Cinder and OpenFrameworks', target: null, postText: null },
+				{ text: 'the React stack', target: null, postText: null },
+				{ text: 'ARKit and ARCore', target: null, postText: null },
+				{ text: 'Unity and Unreal', target: null, postText: null },
+			]
+		},
+		{
+			name: 'working on',
+			index: 6,
+			options: [
+				{ text: 'Johnson and Johnson heritage museum', target: null, postText: null },
+				{ text: 'Best Friends Animal Society', target: null, postText: null },
+				{ text: 'Illinois Holocaust Museum', target: null, postText: null },
 			]
 		},
 	]
